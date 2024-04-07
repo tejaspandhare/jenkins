2ee70a13dc204437d6e1ec3b351bb52b60c5a03c
tejaspandhare <76597065+tejaspandhare@users.noreply.github.com>
2024-04-07 23:39:26 +0530

    Create pythontest.py

---
+++ pythontest.py
@@ -0,0 +1,14 @@
+pipeline {
+  agent { label 'linux' }
+  environment {
+   ANSIBLE_PRIVATE_KEY=credentials('automation-key') 
+  }
+  stages {
+    stage('Hello') {
+      steps {
+        sh 'python3 smiley.py'
+        
+      }
+    }
+  }
+}
