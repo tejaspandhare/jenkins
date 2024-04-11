65a7c1f3ff83ea98e1e24604f4d90cc1ed773079
tejaspandhare <76597065+tejaspandhare@users.noreply.github.com>
2024-04-11 12:02:11 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,2 +1,9 @@
-messages = input("message")
-print ("Hello World!")
+import sys
+
+def main():
+    message = sys.argv[1]
+    print("Received message:", message)
+    print("Hello World!")
+
+if __name__ == "__main__":
+    main()
