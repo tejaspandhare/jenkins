366df4257a5e4621c72cb617a4b19c705a524f3f
tejaspandhare <76597065+tejaspandhare@users.noreply.github.com>
2024-04-13 09:45:10 +0530

    Update pythontest.py

--- pythontest.py
+++ pythontest.py
@@ -5,7 +5,6 @@ pipeline {
         stage('Run Python Script') {
             steps {
                 script {
-                    def message = "Your input message here" //  Provide your input message here
                     sh "echo '${message}' | /usr/bin/python3 smiley.py"
                 }
             }
