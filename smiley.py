7db8bae538eef1ace90c2862196c5bd710074d20
tejaspandhare <76597065+tejaspandhare@users.noreply.github.com>
2024-04-13 09:25:25 +0530

    Update smiley.py

--- smiley.py
+++ smiley.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
+import sys
+
 def emoji_converter(message):
-        split_message = message.split(" ")
-        emojis_set = {
+    split_message = message.split(" ")
+    emojis_set = {
         ":)" : "🙂",
         ":(" : "😔"
-          }
-        output = " "
-        for i in split_message:
-            output += emojis_set.get(i, i) + " "
-        return output
+    }
+    output = " "
+    for i in split_message:
+        output += emojis_set.get(i, i) + " "
+    return output
 
-
-message = input("> ")
+# Read message from standard input
+message = sys.stdin.readline().strip()
 print(emoji_converter(message))
